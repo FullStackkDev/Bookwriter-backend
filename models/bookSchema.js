f1e54f92fec3f329e9f57e31c54150f01a2c72dc
fullstackkdev <fullstackkdev@gmail.com>
2023-10-06 15:15:35 +0500

    Make book schema and model

---
+++ models/bookSchema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const book = mongoose.model("book", bookSchema);
+
+export default book;
