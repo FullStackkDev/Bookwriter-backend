aaadff7cacb2df0441321941c861769e04814531
fullstackkdev <fullstackkdev@gmail.com>
2023-10-06 15:10:53 +0500

    Make user schema and model

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,1 +1,30 @@
 // user model
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
