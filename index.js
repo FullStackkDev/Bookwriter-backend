3c410d4ba847fb1de7f5a399c2f9a596675a49ef
fullstackkdev <fullstackkdev@gmail.com>
2023-10-06 11:57:10 +0500

    Make server. Add a function that connect our app with database.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// index file 
-// add sever
+// index file
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./db/config.js";
+
+const app = express();
+dotenv.config();
+connectDB();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Nodejs api connected successfully!!!"));
+
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
+});
