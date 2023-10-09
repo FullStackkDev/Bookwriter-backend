20c5ca4cc2ab6d27de4fc399dcb8a16e68344460
fullstackkdev <fullstackkdev@gmail.com>
2023-10-09 12:54:29 +0500

    Add code for creating user in create user function.

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,35 @@ const getUser = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-  res.send("cretae user");
+  try {
+    const { first_name, last_name, email, password } = req.body;
+
+    if (!first_name || !last_name || !email || !password) {
+      return res.status(404).json({
+        message: "Please Fill all Fields",
+        success: false,
+      });
+    }
+
+    //check if user already exist based on email
+    const isUserExist = await User.findOne({ email });
+    if (isUserExist) {
+      return res.status(404).json({
+        message: "User Already Exist",
+        success: false,
+      });
+    }
+
+    const user = await User.create({ first_name, last_name, email, password });
+
+    return res.status(201).json({
+      message: "User Created Successfully",
+      payload: user,
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message, success: false });
+  }
 };
 
 const updateUser = async (req, res) => {
