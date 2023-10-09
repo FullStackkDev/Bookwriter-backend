fc06a29ec8ad92db649bcd074a98129bac3560d5
fullstackkdev <fullstackkdev@gmail.com>
2023-10-09 16:23:30 +0500

    Add middleware for cors.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index file
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./db/config.js";
 import bookRouter from "./routes/book.js";
 import userRouter from "./routes/user.js";
@@ -11,6 +12,7 @@ const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json({ limit: "2mb" }));
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Nodejs api connected successfully!!!"));
