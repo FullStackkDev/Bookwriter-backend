5c619c1c13d7f593308a044f4be985053688ebec
fullstackkdev <fullstackkdev@gmail.com>
2023-10-09 12:52:36 +0500

    Add required in password field and also add the password hashing logic.

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,6 @@
 // user model
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -18,12 +19,26 @@ const userSchema = mongoose.Schema(
     },
     password: {
       type: String,
+      required: true,
     },
   },
   {
     timestamps: true,
   }
 );
+
+// WE ARE HASHING THE PASSWORD
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  if (this.password) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  next();
+});
 
 const user = mongoose.model("user", userSchema);
 
