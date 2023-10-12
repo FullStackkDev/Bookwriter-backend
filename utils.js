df712945d743f93d482a7295784d224c1f4838dd
fullstackkdev <fullstackkdev@gmail.com>
2023-10-12 18:34:18 +0500

    Fix regex for name.

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
-const nameRegex = /^[A-Za-z][A-Za-z0-9'-]*$/; //This regular expression ensures that the name doesn't start with a number and only allows letters, hyphens, and single quotes in the rest of the name.
+const nameRegex = /^[A-Za-z][A-Za-z0-9' -]*$/;
+//This regular expression ensures that the name doesn't start with a number and only allows letters, hyphens, and single quotes in the rest of the name.
 
 const phoneRegex = /^\+\d{2}-\d{10}$/; //This regular expression will ensure that the phone number starts with a plus sign, followed by two digits, a hyphen, and then ten digits, and nothing else is allowed before or after this pattern in the string.
 
